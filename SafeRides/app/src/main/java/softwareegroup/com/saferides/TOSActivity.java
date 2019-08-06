d5c26ccc58bee9a1c27b042a4388350564fd7be5
James Caldwell <jcaldwell2@radford.edu>
2019-08-06 01:46:08 -0400

    Adding Safe Rides Android app from Software Engineering 1, from Fall 2015

---
+++ SafeRides/app/src/main/java/softwareegroup/com/saferides/TOSActivity.java
@@ -0,0 +1,13 @@
+package softwareegroup.com.saferides;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TOSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tos);
+    }
+}
